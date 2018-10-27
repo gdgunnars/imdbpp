7fb8e0eac61a4686aa4e596c3f5e127430dcd98b
Andri Andresson <andriandresson@gmail.com>
2018-10-27 21:42:18 +0000

    added poster + dummy text to backdrop component

---
+++ frontend/src/components/backdrop/fakeShadow.backdrop.component.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+import { LinearGradient } from 'expo';
+
+const ShadowContainer = styled.View`
+  position: absolute;
+  height: ${props => props.height};
+  left: 0;
+  right: 0;
+`;
+
+const ShadowTop = styled(ShadowContainer)`
+  top: 0;
+`;
+
+const ShadowBottom = styled(ShadowContainer)`
+  bottom: 0;
+`;
+
+const FakeShadow = ({ position, height }) => {
+  const style = { height: '100%', width: '100%' };
+  const colors = ['#141414', 'rgba(20,20,20, 0.25)', 'rgba(20,20,20,0)'];
+  if (position === 'top') {
+    return (
+      <ShadowTop height={height}>
+        <LinearGradient style={style} colors={colors} />
+      </ShadowTop>
+    );
+  }
+  return (
+    <ShadowBottom height={height}>
+      <LinearGradient style={style} colors={colors.reverse()} />
+    </ShadowBottom>
+  );
+};
+
+export default FakeShadow;
--- frontend/src/components/backdrop/image.backdrop.component.js
+++ frontend/src/components/backdrop/image.backdrop.component.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { LinearGradient } from 'expo';
+import FakeShadow from './fakeShadow.backdrop.component';
 
 const Container = styled.View`
   height: ${props => props.height};
@@ -12,45 +12,13 @@ const BackdropImage = styled.Image`
   height: ${props => props.height};
 `;
 
-const ShadowContainer = styled.View`
-  position: absolute;
-  height: ${props => props.height};
-  left: 0;
-  right: 0;
-`;
-
-const ShadowTop = styled(ShadowContainer)`
-  top: 0;
-`;
-
-const ShadowBottom = styled(ShadowContainer)`
-  bottom: 0;
-`;
-
-const FakeShadow = ({ position, height }) => {
-  const style = { height: '100%', width: '100%' };
-  const colors = ['#141414', 'rgba(20,20,20, 0.25)', 'rgba(20,20,20,0)'];
-  if (position === 'top') {
-    return (
-      <ShadowTop height={height}>
-        <LinearGradient style={style} colors={colors} />
-      </ShadowTop>
-    );
-  }
-  return (
-    <ShadowBottom height={height}>
-      <LinearGradient style={style} colors={colors.reverse()} />
-    </ShadowBottom>
-  );
-};
-
 const BackdropImageContainer = (props) => {
   const { url, height } = props;
   return (
     <Container height={height}>
       <BackdropImage source={{ uri: url }} height={height} />
-      <FakeShadow position="top" height={height * 0.8} />
-      <FakeShadow position="bottom" height={height * 0.8} />
+      <FakeShadow position="top" height={height * 0.9} />
+      <FakeShadow position="bottom" height={height * 0.9} />
     </Container>
   );
 };
