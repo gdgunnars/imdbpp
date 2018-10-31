6bf51a804b7c02d060c4008964274988b592e419
Andri Andresson <andriandresson@gmail.com>
2018-10-31 16:29:24 +0000

    working on tvShow detail route

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,16 @@
-import express from 'express';
-import cors from 'cors';
-import trendingRoute from './routes/trending.route';
+import express from "express";
+import cors from "cors";
+import trendingRoute from "./routes/trending.route";
+import tvShowRoute from "./routes/tvShow.route";
 const app = express();
 const port = 3000;
 
 app.use(cors());
-app.use('/trending', trendingRoute);
+app.use("/trending", trendingRoute);
+app.use("/tvshow", tvShowRoute);
 
-app.get('/', function(req, res) {
-  res.send('API up and running...');
+app.get("/", function(req, res) {
+  res.send("API up and running...");
 });
 
 app.listen(port, () => {
--- backend/routes/tvShow.route.js
+++ backend/routes/tvShow.route.js
@@ -1,5 +1,8 @@
-// Todo: Construct route for details:
+import express from "express";
+import axios from "axios";
+import * as config from "../config.js";
 
+// Todo: Construct route for details:
 
 // Get Details: https://api.themoviedb.org/3/tv/79242?api_key=c676ad1c06defe8fd668a42c4b030547&language=en-US
 
@@ -7,3 +10,32 @@
 
 // Get Similar Tv Shows: https://api.themoviedb.org/3/tv/79242/similar?api_key=c676ad1c06defe8fd668a42c4b030547&language=en-US&page=1
 
+const router = express.Router();
+router.route("/:tvId").get(async (req, res) => {
+  const { tvId } = req.params;
+  if (!tvId) {
+    res.json({ message: "Missing tvId" });
+  }
+  try {
+    console.time("test");
+    const getDetails = axios.get(
+      `${config.getBasePath()}/tv/${tvId}?api_key=${config.getApiKey()}`
+    );
+    const getCredits = axios.get(
+      `${config.getBasePath()}/tv/${tvId}/credits?api_key=${config.getApiKey()}&language=en-US`
+    );
+    const getSimilar = axios.get(
+      `${config.getBasePath()}/tv/${tvId}/similar?api_key=${config.getApiKey()}&language=en-US&page=1`
+    );
+    const allData = await Promise.all([getDetails, getCredits, getSimilar]);
+    console.timeEnd("test");
+    const mappedData = allData.map(item => item.data);
+    // Todo: parse data and take what i need.
+    res.json({ message: "I got something" });
+  } catch (error) {
+    console.log(error);
+    res.json({ message: "I got err" });
+  }
+});
+
+export default router;
