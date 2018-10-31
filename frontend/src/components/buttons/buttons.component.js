f2e544b095b00f7893913a821c2554278461e02b
Andri Andresson <andriandresson@gmail.com>
2018-10-31 19:30:24 +0000

    changin play & pause btns from FontAwesome to Material

--- frontend/src/components/buttons/buttons.component.js
+++ frontend/src/components/buttons/buttons.component.js
@@ -23,9 +23,8 @@ const BookmarkIconContainer = styled.View`
 `;
 
 class Buttons extends PureComponent {
-
   constructor(props) {
-    super(props)
+    super(props);
   }
 
   getButton(type) {
@@ -38,32 +37,33 @@ class Buttons extends PureComponent {
 
     switch (type) {
       case 'play':
-        return <Icon.FontAwesome name={'play-circle-o'} color={whiteColor} size={iconSize} />
+        return <Icon.MaterialIcons name="play-circle-outline" color={whiteColor} size={iconSize} />;
       case 'pause':
-        return <Icon.FontAwesome name={'pause-circle-o'} color={whiteColor} size={iconSize} />
+        return (
+          <Icon.MaterialIcons name="pause-circle-outline" color={whiteColor} size={iconSize} />
+        );
       case 'camera':
-        return <Icon.FontAwesome name={'camera'} color={whiteColor} size={iconSize} />
+        return <Icon.FontAwesome name="camera" color={whiteColor} size={iconSize} />;
       case 'add':
         return (
           <BookmarkContainer>
-            <Icon.FontAwesome name={'bookmark'} color={whiteColor} size={iconSize} />
+            <Icon.FontAwesome name="bookmark" color={whiteColor} size={iconSize} />
             <BookmarkIconContainer>
-              <Icon.FontAwesome name={'plus'} size={iconSize / reduceSize} />
+              <Icon.FontAwesome name="plus" size={iconSize / reduceSize} />
             </BookmarkIconContainer>
           </BookmarkContainer>
         );
       case 'remove':
         return (
           <BookmarkContainer>
-            <Icon.FontAwesome name={'bookmark'} color={redColor} size={iconSize} />
+            <Icon.FontAwesome name="bookmark" color={redColor} size={iconSize} />
             <BookmarkIconContainer>
-              <Icon.FontAwesome name={'close'} color={whiteColor} size={iconSize / reduceSize} />
+              <Icon.FontAwesome name="close" color={whiteColor} size={iconSize / reduceSize} />
             </BookmarkIconContainer>
           </BookmarkContainer>
         );
       default:
-        return <Icon.FontAwesome name={'question-circle-o'} color={'#ff69b4'} size={iconSizeMissing} />
-
+        return <Icon.FontAwesome name="question-circle-o" color="#ff69b4" size={iconSizeMissing} />;
     }
   }
 
