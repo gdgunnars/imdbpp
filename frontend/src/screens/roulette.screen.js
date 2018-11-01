3b8a5f3c274a24e8d5c71f37fcdeca60144ee580
Andri Andresson <andriandresson@gmail.com>
2018-11-01 12:50:45 +0000

    roulette screen cleanup

--- frontend/src/screens/roulette.screen.js
+++ frontend/src/screens/roulette.screen.js
@@ -1,19 +1,9 @@
 import React, { PureComponent } from 'react';
-import { View } from 'react-native';
-import Buttons from '../components/buttons/index';
 import ScreenContainer from './screen.style';
 
 class RouletteScreen extends PureComponent {
-  test = () => {
-    console.log('Its working John');
-  }
   render() {
-    return <ScreenContainer >
-      <View style={{ margin: 50 }}>
-        <Buttons name={'remove'} size={'55%'} onClick={this.test} />
-      </View>
-    </ScreenContainer>;
-
+    return <ScreenContainer />;
   }
 }
 
