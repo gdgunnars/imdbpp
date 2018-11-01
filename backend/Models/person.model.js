f89a849b073217a48313fd37e9730c22442fde3e
Andri Andresson <andriandresson@gmail.com>
2018-11-01 02:27:42 +0000

    added person.model

--- backend/Models/person.model.js
+++ backend/Models/person.model.js
@@ -2,7 +2,8 @@ const person = ({ character, id, name, gender, profile_path }) => ({
   character,
   id,
   name,
-  profilePath: profile_path
+  profilePath: profile_path,
+  type: "person"
 });
 
 export default person;
