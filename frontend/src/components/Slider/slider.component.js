b729bb8fdf6f8998c715b1a5e423e599e952a1de
Andri Andresson <andriandresson@gmail.com>
2018-11-02 19:40:10 +0000

    Slider now uses simple flatlist for all posters, and snapable slider for everything else

--- frontend/src/components/Slider/slider.component.js
+++ frontend/src/components/Slider/slider.component.js
@@ -1,18 +1,40 @@
 import React, { PureComponent } from 'react';
 import Carousel from 'react-native-carousel-control';
+import { FlatList, View } from 'react-native';
+import styled from 'styled-components';
+import * as DimSize from '../../common/dimensionSize';
+
+const Seperator = styled.View`
+  width: ${DimSize.width('1%')};
+  height: 100%;
+`;
 
 class Slider extends PureComponent {
   render() {
     const { items, seperator, snapWidth } = this.props;
     const sneak = seperator ? snapWidth / 1.5 : 0;
     const currPage = seperator ? Math.round(items.length / 2) : 0;
+
+    if (seperator) {
+      return (
+        <View>
+          <FlatList
+            horizontal
+            data={items}
+            ItemSeparatorComponent={() => <Seperator />}
+            renderItem={({ item }) => item}
+            showsHorizontalScrollIndicator={false}
+          />
+        </View>
+      );
+    }
     return (
       <Carousel
         pageWidth={snapWidth}
         pageStyle={{ alignSelf: 'flex-start' }}
         sneak={sneak}
         currentPage={currPage}
-        swipeThreshold={0.3}
+        swipeThreshold={0.1}
       >
         {items.map(item => item)}
       </Carousel>
