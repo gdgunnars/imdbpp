e5145336f220ed0786a993ba435d877449d5652d
Andri Andresson <andriandresson@gmail.com>
2018-11-06 23:28:29 +0000

    Making sure google credentials are being loaded to process env

---
+++ backend/env.js
@@ -0,0 +1,6 @@
+import path from "path";
+
+process.env["GOOGLE_APPLICATION_CREDENTIALS"] = path.resolve(
+  __dirname,
+  "assignment5lock.json"
+);
--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,15 @@
+import "./env";
 import express from "express";
 import cors from "cors";
 import trendingRoute from "./routes/trending.route";
 import tvShowRoute from "./routes/tvShow.route";
 import searchRoute from "./routes/search.route";
 import movieRoute from "./routes/movie.route";
-import topRatedMoviesRoute from './routes/topRatedMovies.route';
-import topRatedTvRoute from './routes/topRatedTv.route';
-import moviesByGenreRoute from './routes/moviesByGenre.route';
-import tvByGenreRoute from './routes/tvByGenre.route';
-import vision from './routes/vision.route';
+import topRatedMoviesRoute from "./routes/topRatedMovies.route";
+import topRatedTvRoute from "./routes/topRatedTv.route";
+import moviesByGenreRoute from "./routes/moviesByGenre.route";
+import tvByGenreRoute from "./routes/tvByGenre.route";
+import vision from "./routes/vision.route";
 
 const app = express();
 const port = 3000;
@@ -18,17 +19,16 @@ app.use("/trending", trendingRoute);
 app.use("/tvshow", tvShowRoute);
 app.use("/movie", movieRoute);
 app.use("/search", searchRoute);
-app.use('/topratedmovies', topRatedMoviesRoute);
-app.use('/topratedtv', topRatedTvRoute);
-app.use('/moviesgenre', moviesByGenreRoute);
-app.use('/tvgenre', tvByGenreRoute);
-app.use('/vision', vision)
+app.use("/topratedmovies", topRatedMoviesRoute);
+app.use("/topratedtv", topRatedTvRoute);
+app.use("/moviesgenre", moviesByGenreRoute);
+app.use("/tvgenre", tvByGenreRoute);
+app.use("/vision", vision);
 
-app.get('/', function (req, res) {
-  res.send('API up and running...');
+app.get("/", function(req, res) {
+  res.send("API up and running...");
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
-
 });
