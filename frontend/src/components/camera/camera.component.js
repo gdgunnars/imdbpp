5b3943d0dc637833fd46b9f896fd677ece026143
Toti91 <torarinng@gmail.com>
2018-11-07 22:51:27 +0000

    Camera component refactor

--- frontend/src/components/camera/camera.component.js
+++ frontend/src/components/camera/camera.component.js
@@ -1,45 +1,138 @@
 import React from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
-import { Camera, Permissions } from 'expo';
+import styled from 'styled-components';
+import { Text, View } from 'react-native';
+import {
+  Camera, Permissions, ImageManipulator, Icon,
+} from 'expo';
 
-export default class CameraExample extends React.Component {
+const Container = styled.View`
+  flex: 1;
+`;
+
+const Cam = styled.View`
+  flex: 1;
+  background-color: transparent;
+  flex-direction: row;
+  position: relative;
+`;
+
+const Flip = styled.TouchableOpacity`
+  flex: 0.1;
+  alignself: flex-end;
+  alignitems: center;
+`;
+
+const FlipContainer = styled.View`
+  position: absolute;
+  top: 0;
+  width: 100%;
+  justify-content: center;
+  padding-top: 20;
+  padding-right: 20;
+`;
+
+const SnapContainer = styled.View`
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TakePicture = styled.TouchableOpacity`
+  flex: 0.2;
+  align-items: center;
+`;
+
+export default class SearchCamera extends React.Component {
   state = {
     hasCameraPermission: null,
     type: Camera.Constants.Type.back,
   };
 
-  async componentDidMount() {
+  camera = null;
+
+  async componentWillMount() {
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
-    this.setState({ hasCameraPermission: status === 'granted' });
+    this.setState({
+      hasCameraPermission: status === 'granted',
+    });
   }
+
+  snap = async () => {
+    if (this.camera) {
+      let photo = await this.camera.takePictureAsync({ exif: true });
+
+      // We need to orientate the picture correctly based on EXIF metadata
+      // Otherwise it's always in landscape orientation
+      photo = await ImageManipulator.manipulate(
+        photo.uri,
+        [
+          {
+            rotate: -photo.exif.Orientation,
+          },
+          {
+            resize: {
+              width: photo.width,
+              height: photo.height,
+            },
+          },
+        ],
+        { compress: 1 },
+      );
+
+      console.log(photo);
+    }
+  };
+
+  flip = () => {
+    const { type } = this.state;
+
+    this.setState({
+      type:
+        type === Camera.Constants.Type.back
+          ? Camera.Constants.Type.front
+          : Camera.Constants.Type.back,
+    });
+  };
 
   render() {
     const { hasCameraPermission, type } = this.state;
+
     if (hasCameraPermission === null) {
       return <View />;
     }
     if (hasCameraPermission === false) {
-      return <Text>No access to camera</Text>;
+      return <Text>No access to the camera.</Text>;
     }
+
     return (
-      <View style={{ flex: 1 }}>
-        <Camera style={{ flex: 1 }} type={type}>
-          <View style={{ flex: 1, backgroundColor: 'transparent', flexDirection: 'row' }}>
-            <TouchableOpacity
-              style={{ flex: 0.1, alignSelf: 'flex-end', alignItems: 'center' }}
-              onPress={() => {
-                this.setState({
-                  type: type === Camera.Constants.Type.back
-                    ? Camera.Constants.Type.front
-                    : Camera.Constants.Type.back,
-                });
-              }}
-            >
-              <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>` Flip ` </Text>
-            </TouchableOpacity>
-          </View>
+      <Container>
+        <Camera
+          style={{ flex: 1 }}
+          type={type}
+          ref={(ref) => {
+            this.camera = ref;
+          }}
+        >
+          <Cam>
+            <FlipContainer>
+              <Flip onPress={this.flip}>
+                <Icon.Ionicons
+                  name="ios-reverse-camera"
+                  color="white"
+                  size={36}
+                />
+              </Flip>
+            </FlipContainer>
+            <SnapContainer>
+              <TakePicture onPress={this.snap}>
+                <Icon.Ionicons name="ios-camera" color="white" size={64} />
+              </TakePicture>
+            </SnapContainer>
+          </Cam>
         </Camera>
-      </View>
+      </Container>
     );
   }
 }
