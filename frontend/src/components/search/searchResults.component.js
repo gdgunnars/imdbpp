a6e3b034fb1d51849db2388dbf45390ccf8f59db
Andri Andresson <andriandresson@gmail.com>
2018-11-08 00:37:03 +0000

    Finishing up searchResults

--- frontend/src/components/search/searchResults.component.js
+++ frontend/src/components/search/searchResults.component.js
@@ -2,12 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 import { LinearGradient } from 'expo';
 import { Header } from 'react-navigation';
-import { DimSize, MediaLink } from '../../common';
+import { DimSize, MediaLink, Theme } from '../../common';
 import { navigate } from '../../navigation';
 import Slider from '../Slider';
 import Poster from '../poster';
 import SearchItem from './searchItem.component';
 import { addItemToRecentSearches } from '../../services';
+import { View } from '../../general';
 
 const NothingFound = styled.Text`
   color: #fefefe;
@@ -15,22 +16,26 @@ const NothingFound = styled.Text`
   text-align: center;
 `;
 
-const SearchResultWrapper = styled.View`
-  margin-top: ${Header.HEIGHT + DimSize.windowSidesPadding() * 2};
-  padding-top: ${DimSize.windowSidesPadding()};
+const SearchResultWrapper = styled.ScrollView`
+  margin-top: ${Theme.sizes.statusBar.height + Theme.sizes.navBar.height};
   margin-left: ${DimSize.windowSidesPadding()};
   margin-right: ${DimSize.windowSidesPadding()};
+  padding-bottom: ${Theme.sizes.spaces.content.large.bottom};  
 `;
 
 const gradientStyle = {
   height: DimSize.height('80%'),
   width: DimSize.width('100%'),
-  position: 'absolute',
-  top: 0,
-  left: 0,
-  right: 0,
-  opacity: 0.55,
+  opacity: 0.7,
 };
+
+const LastItem = styled.View`
+  margin-bottom: ${Theme.sizes.spaces.content.large.bottom};
+`
+
+const SectionView = styled.View`
+  margin-top: ${Theme.sizes.spaces.content.large.top};
+`;
 
 const SearchResultsContainer = styled.View`
   flex: 1;
@@ -38,11 +43,25 @@ const SearchResultsContainer = styled.View`
 
 const TopResult = styled(NothingFound)`
   font-weight: bold;
+  padding-top: ${Theme.sizes.spaces.content.large.top};
 `;
 
-const KnownForTitle = styled(TopResult)`
-  margin-top: ${DimSize.windowSidesPadding()};
-  margin-bottom: ${DimSize.windowSidesPadding()};
+const SubTitle = styled(NothingFound)`
+  font-weight: bold;
+  margin-top: ${Theme.sizes.spaces.content.medium.top};
+  margin-bottom: ${Theme.sizes.spaces.content.small.bottom};
+`;
+
+const MirrorScroll = styled.ScrollView`
+  height: ${Theme.sizes.window.height * 2};
+`;
+
+const MirrorScrollAbsoluteWrapper = styled.View`
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
 `;
 
 const createSearchResItemLink = (item) => {
@@ -65,44 +84,97 @@ const renderKnownFor = tvShows => tvShows.map((item) => {
   );
 });
 
-const SearchResults = (props) => {
-  const { searchResults } = props;
-  if (!searchResults) {
+const renderOtherContent = list => list
+  .sort((a, b) => (a.data.popularity > b.popularity ? -1 : 1))
+  .filter(item => item.data.length > 0)
+  .map(item => (
+    <SectionView key={item.subTitle}>
+      <SubTitle>{item.subTitle}</SubTitle>
+      {item.data.map((innerItem) => {
+        const link = createSearchResItemLink(innerItem);
+        return <SearchItem key={innerItem.id} media={innerItem} onPress={link} />;
+      })}
+    </SectionView>
+  ));
+
+const FillContent = styled.View``;
+
+class SearchResults extends React.PureComponent {
+  mirrorScrollRef = React.createRef();
+
+  state = {
+    mirrorScrollHeight: 0,
+  };
+
+  setScrollViewHeight = (event) => {
+    const { height } = event.nativeEvent.layout;
+    const { mirrorScrollHeight } = this.state;
+    console.log('HEIGHT: ', mirrorScrollHeight);
+    console.log('containerHeiught: ', height);
+    this.setState({
+      mirrorScrollHeight: height,
+    });
+  };
+
+  handleScrollViewScroll = (event) => {
+    const {y} = event.nativeEvent.contentOffset;
+    this.mirrorScrollRef.current.scrollTo({y});
+  };
+
+  render() {
+    const { searchResults } = this.props;
+    if (!searchResults) {
+      return (
+        <SearchResultsContainer>
+          <SearchResultWrapper>
+            <NothingFound>Nothing found..</NothingFound>
+          </SearchResultWrapper>
+        </SearchResultsContainer>
+      );
+    }
+    const {
+      topResult, person, movie, tv,
+    } = searchResults;
+
+    const { color = '#5CE9AC' } = topResult;
+    const { mirrorScrollHeight } = this.state;
+    const colors = [color, 'transparent'];
+    const posterSnapWidh = Math.round(DimSize.height('32%') * 0.7 + DimSize.width('2%'));
+    const TopResLink = createSearchResItemLink(topResult.data);
+
     return (
       <SearchResultsContainer>
-        <SearchResultWrapper>
-          <NothingFound>Nothing found..</NothingFound>
+        <MirrorScrollAbsoluteWrapper>
+          <MirrorScroll ref={this.mirrorScrollRef} style={{ height: mirrorScrollHeight }} scrollEnabled={false}>
+            <LinearGradient locations={[0.1, 1]} style={gradientStyle} colors={colors} />
+            <FillContent style={{ height: mirrorScrollHeight * 2}} />
+          </MirrorScroll>
+        </MirrorScrollAbsoluteWrapper>
+        <SearchResultWrapper
+          showsVerticalScrollIndicator={false}
+          onScroll={this.handleScrollViewScroll}
+          onLayout={this.setScrollViewHeight}
+        >
+          <TopResult>Top result</TopResult>
+          <SearchItem media={topResult.data} onPress={TopResLink} />
+          {topResult.data.knownFor && <SubTitle>{`Featuring ${topResult.data.name}`}</SubTitle>}
+          {topResult.data.knownFor && (
+            <Slider
+              snapWidth={posterSnapWidh}
+              items={renderKnownFor(topResult.data.knownFor)}
+              seperator
+            />
+          )}
+          {renderOtherContent([
+            { subTitle: 'People', data: person.data },
+            { subTitle: 'Movies', data: movie.data },
+            { subTitle: 'Tv-Shows', data: tv.data },
+          ])}
+          <LastItem />
         </SearchResultWrapper>
       </SearchResultsContainer>
     );
   }
-  const {
-    topResult, person, movie, tv,
-  } = searchResults;
-
-  const { color = '#5CE9AC' } = topResult;
-  const colors = [color, 'transparent'];
-  const posterSnapWidh = Math.round(DimSize.height('32%') * 0.7 + DimSize.width('2%'));
-  const TopResLink = createSearchResItemLink(topResult.data);
-  return (
-    <SearchResultsContainer>
-      <LinearGradient locations={[0.1, 1]} style={gradientStyle} colors={colors} />
-      <SearchResultWrapper>
-        <TopResult>Top result</TopResult>
-        <SearchItem media={topResult.data} onPress={TopResLink} />
-        {topResult.data.knownFor && (
-          <KnownForTitle>{`Featuring ${topResult.data.name}`}</KnownForTitle>
-        )}
-        {topResult.data.knownFor && (
-          <Slider
-            snapWidth={posterSnapWidh}
-            items={renderKnownFor(topResult.data.knownFor)}
-            seperator
-          />
-        )}
-      </SearchResultWrapper>
-    </SearchResultsContainer>
-  );
-};
+}
 
 export default SearchResults;
