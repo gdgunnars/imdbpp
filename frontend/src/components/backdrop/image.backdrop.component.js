4cf2dfd19849affab1a09aa94a1ecf955054acbe
Andri Andresson <andriandresson@gmail.com>
2018-11-09 06:36:52 +0000

    attempting to fix issue #34, needs review

--- frontend/src/components/backdrop/image.backdrop.component.js
+++ frontend/src/components/backdrop/image.backdrop.component.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Theme } from '../../common';
+import OverlayEdges from '../../../assets/images/overlay-edges.png';
 import FakeShadow from './fakeShadow.backdrop.component';
 
 const Container = styled.View`
   height: ${props => props.height};
   width: 100%;
   background-color: #141414;
+  background-color: #fefefe;
   position: relative;
 `;
 
 const BackdropImage = styled.Image`
   height: ${props => props.height};
+  width: ${Theme.sizes.window.width};
+`;
+
+const OverlayImage = styled(BackdropImage)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
 `;
 
 const BackdropImageContainer = (props) => {
@@ -18,8 +30,8 @@ const BackdropImageContainer = (props) => {
   return (
     <Container height={height}>
       <BackdropImage source={{ uri: url }} height={height} resizeMode="cover" />
-      <FakeShadow position="top" height={height * 0.15} />
-      <FakeShadow position="bottom" height={height * 0.4} />
+      <OverlayImage source={OverlayEdges} height={height} resizeMode="stretch" />
+      <FakeShadow height={height * 0.05} position="bot" />
     </Container>
   );
 };
