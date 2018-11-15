1439bc4165d2f570f44532457910d12122ab4af0
Andri Andresson <andriandresson@gmail.com>
2018-11-15 22:06:18 +0000

    added tripple dots after sliced text in ToggleShowMore component

--- frontend/src/components/toggleShowMore/toggleShowMore.component.js
+++ frontend/src/components/toggleShowMore/toggleShowMore.component.js
@@ -2,23 +2,21 @@ import React, { PureComponent } from 'react';
 import { Text, View } from '../../general';
 
 class ToggleShowMore extends PureComponent {
-
-    render() {
-        const { text, active } = this.props;
-        if (text) {
-            let textData = active ? text : text.slice(0, 100);
-            return (
-                <View.rowPadding>
-                    <Text.body1>{textData}</Text.body1>
-                </View.rowPadding>
-            )
-        } else {
-            return (
-                <View.rowPadding>
-                    <Text.body1>No further info available</Text.body1>
-                </View.rowPadding>
-            )
-        }
+  render() {
+    const { text, active } = this.props;
+    if (text) {
+      const textData = active ? text : `${text.slice(0, 100)}...`;
+      return (
+        <View.rowPadding>
+          <Text.body1>{textData}</Text.body1>
+        </View.rowPadding>
+      );
     }
+    return (
+      <View.rowPadding>
+        <Text.body1>No further info available</Text.body1>
+      </View.rowPadding>
+    );
+  }
 }
 export default ToggleShowMore;
