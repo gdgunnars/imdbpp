d6c6ebbd9a9526689400d51196cf7d6baa8e39cf
Andri Andresson <andriandresson@gmail.com>
2018-11-15 14:57:36 +0000

    added randBetween function

--- frontend/src/common/index.js
+++ frontend/src/common/index.js
@@ -4,7 +4,8 @@ import Theme from './theme';
 import MediaLink from './mediaLink';
 import Capitalize from './capitalize';
 import DurationFormat from './durationFormat';
+import RandBetween from './randBetween';
 
 export {
-  DateFormat, DimSize, Theme, MediaLink, Capitalize, DurationFormat,
+  DateFormat, DimSize, Theme, MediaLink, Capitalize, DurationFormat, RandBetween,
 };
---
+++ frontend/src/common/randBetween.js
@@ -0,0 +1,3 @@
+const randBetween = (min, high) => Math.floor(Math.random() * high) + min;
+
+export default randBetween;
