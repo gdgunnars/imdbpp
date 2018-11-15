795ba367858e4703e257b1a84526732d177a71c8
Andri Andresson <andriandresson@gmail.com>
2018-11-15 17:05:26 +0000

    minor changes to discover route

--- backend/routes/discover.route.js
+++ backend/routes/discover.route.js
@@ -7,11 +7,11 @@ const router = express.Router();
 router.route("/").get(async (req, res) => {
   try {
     console.time("bingo");
-    const discoverDataLink = `${config.getBasePath()}/discover/tv/?api_key=${config.getApiKey()}&language=en-US&sort_by=popularity.desc&include_adult=false&page=1`;
+    const discoverDataLink = `${config.getBasePath()}/discover/tv/?api_key=${config.getApiKey()}&language=en-US&sort_by=popularity.desc&first_air_date_year=2018&page=1&include_null_first_air_dates=false`;
     const { data } = await axios.get(discoverDataLink);
     const { results } = data;
 
-    res.json(populateMedia(results).filter(item => item.backdropPath));
+    res.json(populateMedia(results).filter(item => item.posterPath));
   } catch (error) {
     console.log(error);
     res.json({ message: "SHIT FAILED" });
