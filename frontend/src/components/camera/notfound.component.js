44ffa0c42ac0d9e14b6677f6ee862d09b63796af
Andri Andresson <andriandresson@gmail.com>
2018-11-15 20:33:31 +0000

    camera notFound rework

--- frontend/src/components/camera/notfound.component.js
+++ frontend/src/components/camera/notfound.component.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Icon } from 'expo';
 import { navigate } from '../../navigation';
-import { Theme } from '../../common';
+import { Theme, DimSize } from '../../common';
+import { Text, View } from '../../general';
 
 const Container = styled.View`
   flex: 1;
@@ -10,38 +12,64 @@ const Container = styled.View`
   background-color: ${Theme.colors.background.dark};
 `;
 
-const Face = styled.Text`
-  font-size: ${Theme.sizes.text.enormous};
-  color: ${Theme.colors.text.light};
+const BtnContainer = styled.View`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-top: ${Theme.sizes.spaces.content.large.top};
+  padding-top: ${Theme.sizes.spaces.content.medium.top};
+  padding-top: ${Theme.sizes.spaces.content.medium.top};
+  padding-left: ${Theme.sizes.spaces.window.left};
+  padding-right: ${Theme.sizes.spaces.window.right};
+  width: ${Theme.sizes.window.width};
 `;
 
-const Text = styled.Text`
-  padding: 14px;
-  font-size: ${Theme.sizes.text.huge};
-  color: ${Theme.colors.text.default};
-`;
-
-const Button = styled.TouchableOpacity`
-  background-color: ${props => (props.back ? Theme.colors.background.movie : Theme.colors.background.tv)};
-  padding: 10px;
-  border-radius: 6px;
-  margin-top: 14px;
-`;
-
-const ButtonText = styled.Text`
-  font-size: ${Theme.sizes.text.large};
-  color: ${Theme.colors.text.light};
+const BtnWrapper = styled.TouchableOpacity`
+  width: ${Theme.sizes.window.width / 2
+    - Theme.sizes.spaces.window.left
+    - Theme.sizes.spaces.content.small.left};
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  height: 44;
+  padding-left: ${Theme.sizes.spaces.content.medium.left};
+  padding-right: ${Theme.sizes.spaces.content.medium.right};
+  padding-top: ${Theme.sizes.spaces.content.medium.top};
+  padding-bottom: ${Theme.sizes.spaces.content.medium.bottom};
+  background-color: ${Theme.colors.background.default};
+  border-radius: ${DimSize.width('1%')};
 `;
 
 export default () => (
   <Container>
-    <Face>-_-</Face>
-    <Text>Nothing found</Text>
-    <Button onPress={() => navigate({ routeName: 'Camera' })}>
-      <ButtonText>Try another image?</ButtonText>
-    </Button>
-    <Button onPress={() => navigate({ routeName: 'Search' })} back>
-      <ButtonText>Back to search</ButtonText>
-    </Button>
+    <View.column justifyContent="center" alignItems="center" stretch>
+      <Icon.Entypo
+        style={{ marginBottom: Theme.sizes.text.large }}
+        name="emoji-neutral"
+        color={Theme.colors.text.default}
+        size={Theme.sizes.text.enormous}
+      />
+      <Text.body2 color="default">Nothing found</Text.body2>
+      <BtnContainer>
+        <BtnWrapper onPress={() => navigate({ routeName: 'Camera' })}>
+          <Icon.Ionicons
+            style={{ marginRight: Theme.sizes.spaces.content.large.left }}
+            name="ios-camera"
+            color={Theme.colors.text.default}
+            size={44}
+          />
+          <Text.caption>Try again</Text.caption>
+        </BtnWrapper>
+        <BtnWrapper onPress={() => navigate({ routeName: 'Search' })}>
+          <Icon.FontAwesome
+            style={{ marginRight: Theme.sizes.spaces.content.large.left }}
+            name="search"
+            color={Theme.colors.text.default}
+            size={28}
+          />
+          <Text.caption>Back to search</Text.caption>
+        </BtnWrapper>
+      </BtnContainer>
+    </View.column>
   </Container>
 );
