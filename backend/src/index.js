2704081d5d101d629c4e9e88c6596aeb36b12364
Gunnar Davíð <gdg@trustpilot.com>
2018-11-17 17:16:15 +0100

    Added logging with morgan to console and file

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,10 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import polyfill from "@babel/polyfill";
+import fs from 'fs';
+import morgan from 'morgan';
+import path from 'path';
+import rfs from 'rotating-file-stream';
 import trendingRoute from "./routes/trending.route";
 import tvShowRoute from "./routes/tvShow.route";
 import searchRoute from "./routes/search.route";
@@ -18,9 +22,25 @@ import discoverRoute from "./routes/discover.route";
 const app = express();
 const port = 3000;
 
+// Enable Cross origin.
 app.use(cors());
+// Add body parsing and upp the body size limit.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({ limit: "15mb" }));
+// Add logging both to console and file.
+const logDirectory = path.join(__dirname, 'log')
+// ensure log directory exists
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory)
+// create a rotating write stream
+const accessLogStream = rfs('access.log', {
+  interval: '7d', // rotate weekly
+  path: logDirectory
+});
+// setup the logger
+app.use(morgan('combined', { stream: accessLogStream }));
+// And log to console.
+app.use(morgan('dev'));
+
 app.use("/trending", trendingRoute);
 app.use("/tvshow", tvShowRoute);
 app.use("/movie", movieRoute);
